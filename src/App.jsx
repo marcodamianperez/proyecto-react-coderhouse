e0bd7c59fd091ba81e1306b3055fae623f07b5bd
Marco Pérez <marcodamianperez@gmail.com>
2023-06-26 23:40:08 -0300

    agregar renderizado condicional del botón 'Finalizar compra'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             element={<ItemListContainer />}
           />
           <Route path="/item/:idItem" element={<ItemDetailContainer />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,6 @@
 import ItemCount from "../ItemCount/ItemCount";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const ItemDetail = ({
   name,
@@ -11,23 +13,31 @@ const ItemDetail = ({
   stock,
   sinopsis,
 }) => {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <div className="container">
       <article className="book">
         <div className="book__img">
           <div className="book__img-container">
-            <img src={picture} alt={`${name} cover`} />
+            {picture && <img src={picture} alt={`${name} cover`} />}
           </div>
         </div>
         <div className="book__header">
           <h1 className="book__header-title">{name}</h1>
           <span className="book__header-author">{author}</span>
           <span className="book__header-price">${price}</span>
-          <ItemCount
-            stock={stock}
-            initial={1}
-            onAdd={(quantity) => console.log(`Cantidad agregada: ${quantity}`)}
-          />
+          {quantity > 0 ? (
+            <NavLink className="btn btn__solid" to={"/cart"}>
+              Finalizar compra
+            </NavLink>
+          ) : (
+            <ItemCount
+              stock={stock}
+              initial={1}
+              onAdd={(quantity) => setQuantity(quantity)}
+            />
+          )}
         </div>
         <div className="book__info">
           <h2 className="book__info-title">Sinopsis</h2>
